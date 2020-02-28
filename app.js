e419e6b9cb5adc9fbf726fe7736f3ddd083f46f2
vegodiego <53877309+vegodiego@users.noreply.github.com>
2020-02-27 20:44:28 -0500

    Identación
    se mejora la identación de los documentos

--- app.js
+++ app.js
@@ -8,51 +8,51 @@ randomNumber();
 
 //Jugando..
 $("input").on("keydown",function(e){
-	if(e.which==13){
+    if(e.which==13){
 
-		var fijas=0;
-		var picas=0;
-		var answer=$(this).val();
+        var fijas=0;
+        var picas=0;
+        var answer=$(this).val();
 
-		//validación de caracteres repetidos y longitud adecuada en la entrada
-		if(answer.length!=4 || charRepeats(answer)==false){
-			//manifestación de error
-			$("input").css("background","red");
-			$("h4").css("color","red");
-		}
-		else{
-			//funcion lógica que evalua el ingreso del usurio
-			$("input").css("background","transparent");
-			$("h4").css("color","black");
-			for (var i = 0; i < number.length; i++) {
-				if(answer.indexOf(number[i])!=-1){
-					picas++;
-					if(number.indexOf(number[i])==answer.indexOf(number[i])){
-						picas--;
-						fijas++;
-					}
-				}	
-			}
-			$("tbody").prepend("<tr><td>"+answer+"</td><td>"+picas+"</td><td>"+fijas+"</td></tr>")
-			$("input").val(null);
+        //validación de caracteres repetidos y longitud adecuada en la entrada
+        if(answer.length!=4 || charRepeats(answer)==false){
+            //manifestación de error
+            $("input").css("background","red");
+            $("h4").css("color","red");
+        }
+        else{
+            //funcion lógica que evalua el ingreso del usurio
+            $("input").css("background","transparent");
+            $("h4").css("color","black");
+            for (var i = 0; i < number.length; i++) {
+                if(answer.indexOf(number[i])!=-1){
+                    picas++;
+                    if(number.indexOf(number[i])==answer.indexOf(number[i])){
+                        picas--;
+                        fijas++;
+                    }
+                }   
+            }
+            $("tbody").prepend("<tr><td>"+answer+"</td><td>"+picas+"</td><td>"+fijas+"</td></tr>")
+            $("input").val(null);
 
-			//mensaje de felicitaciones y reinicio del juego
-			if(fijas==4){
-				$(".dark").show();
-				$(".alert").show();
-			}
-		}
-	}
+            //mensaje de felicitaciones y reinicio del juego
+            if(fijas==4){
+                $(".dark").show();
+                $(".alert").show();
+            }
+        }
+    }
 });
 
 
 //Jugar nuevamente
 $("button").on("click", function(){
-	$(".dark").hide();
-	$(".alert").hide();
-	$("tbody tr").remove();
-	number="";
-	randomNumber();
+    $(".dark").hide();
+    $(".alert").hide();
+    $("tbody tr").remove();
+    number="";
+    randomNumber();
 });
 
 
@@ -64,16 +64,16 @@ $("button").on("click", function(){
 
 // funcion para generar el número aleatorio number
 function randomNumber(){
-	for (var i = 0; i < 4; i++) {
-		var noRepeat=false;
-		while(noRepeat==false){
-			var digito=Math.floor(Math.random()*10).toString();
-			if(number.indexOf(digito)==-1){
-				number+=digito;
-				noRepeat=true;
-			}
-		}
-	}
+    for (var i = 0; i < 4; i++) {
+        var noRepeat=false;
+        while(noRepeat==false){
+            var digito=Math.floor(Math.random()*10).toString();
+            if(number.indexOf(digito)==-1){
+                number+=digito;
+                noRepeat=true;
+            }
+        }
+    }
 }
 
 //funcion que valida que no hayan caracteres repetidos
